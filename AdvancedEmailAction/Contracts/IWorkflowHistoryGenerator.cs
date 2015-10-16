e8fb8b9ddc83ffc1e2f1d003c4b21ecd286ae7b2
sobek1985 <sobek1985@gmail.com>
2015-10-16 15:43:19 +0100

    Include site name if available in email

--- AdvancedEmailAction/Contracts/IWorkflowHistoryGenerator.cs
+++ AdvancedEmailAction/Contracts/IWorkflowHistoryGenerator.cs
@@ -6,7 +6,7 @@ namespace MikeRobbins.AdvancedEmailAction.Contracts
     public interface IWorkflowHistoryGenerator
     {
         string CreateItemWorkflowHistoryHtml(string bodyText, WorkflowHistoryItem workflowHistoryItem, Item emailActionItem, Item workflowItem);
-        string ReplaceHtmlVariables(string bodyText, WorkflowHistoryItem workflowHistoryItem, string workflowHistory, string commands, string comments);
+        string ReplaceHtmlVariables(string bodyText, WorkflowHistoryItem workflowHistoryItem, string workflowHistory, string commands, string comments, string siteName);
         string GetWorkflowHistoryTableData(Item emailAction, WorkflowHistoryItem workflowHistoryItem, Item workflowItem);
     }
 }
